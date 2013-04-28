1eab545df820ca36967c5c6a482bc16d943df01f
Chris Snyder <kb1rma@arrl.net>
2013-04-28 11:23:57 -0400

    No image found message

--- app/views/sexy/index.html.php
+++ app/views/sexy/index.html.php
@@ -1,21 +1,25 @@
-<?php $imageUrl = '/images/' . $sexies->file_name ?>
-<div class="large-7 columns">
-	<a class="image-link" href="<?= $imageUrl ?>" style="background-image:url(<?= $imageUrl ?>)">
-		<img src="<?= $imageUrl ?>" />
-	</a>
-</div>
-<div class="large-5 columns">
-	<form>
-		<div class="row collapse">
-			<div class="small-9 columns">
-				<input type="text" placeholder="comment">
+<?php if ( ! empty($sexies) ) : ?>
+	<?php $imageUrl = '/images/' . $sexies->file_name ?>
+	<div class="large-7 columns">
+		<a class="image-link" href="<?= $imageUrl ?>" style="background-image:url(<?= $imageUrl ?>)">
+			<img src="<?= $imageUrl ?>" />
+		</a>
+	</div>
+	<div class="large-5 columns">
+		<form>
+			<div class="row collapse">
+				<div class="small-9 columns">
+					<input type="text" placeholder="comment">
+				</div>
+				<div class="small-3 columns">
+					<input type="submit" class="button prefix" value="Post" />
+				</div>
 			</div>
-			<div class="small-3 columns">
-				<input type="submit" class="button prefix" value="Post" />
-			</div>
-		</div>
-	</form>
-</div>
+		</form>
+	</div>
+<?php else : ?>
+	<p class="text-center"><em>No image found</em></p>
+<?php endif ?>
 
 <?php $this->scripts($this->html->script('dragndrop.js') ) ?>
 <?php $this->scripts($this->html->script('jquery.filedrop.js') ) ?>
